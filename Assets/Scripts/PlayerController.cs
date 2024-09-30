2dd789f554131349e2b54cfce044d9f1e74ea75b
Fahim <139593307+maybe-fahim@users.noreply.github.com>
2024-09-30 10:44:22 +0100

    Updated playercontroller script to include ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
 
-    public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 3;
+    public TextMeshProUGUI scoreText;
+    public TestMeshProUGUI winText;
 
     void Start()
     {
         count = 0;
-    }
-
-    void OnMove(InputValue value)
-    {
-        moveValue = value.Get<Vector2>();
+        winText.text = "";
+        SetCountText();
     }
 
     void FixedUpdate()
@@ -32,6 +32,16 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups)
+        {
+            winText.text = "You win!";
         }
     }
 }
